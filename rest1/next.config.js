cb96d1f9a76668284e9b4ccc872c19e81bd02aeb
monis0831 <169075425+monis0831@users.noreply.github.com>
2025-11-09 07:48:21 +0530

    pushed

--- rest1/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: { serverActions: { allowedOrigins: ['*'] } },
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
-};
-module.exports = nextConfig;
