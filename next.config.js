e491dcf4afead626d606301ad9d6ee2e520bcf30
monis0831 <169075425+monis0831@users.noreply.github.com>
2025-11-09 22:15:17 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: { serverActions: { allowedOrigins: ['*'] } },
-  typescript: { ignoreBuildErrors: false },
-  eslint: { ignoreDuringBuilds: true },
-};
-module.exports = nextConfig;
